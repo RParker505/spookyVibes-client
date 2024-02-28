6d9ae4f8b85184799cfd8163ad1d79cf1af946aa
RParker505 <rchiado75@gmail.com>
2024-02-28 16:59:34 -0700

    Remove child details from Genre and Director keys

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -11,15 +11,8 @@ export const MainView = () => {
             Description: "Members of a grieving family each begin to have disturbing, otherworldly experiences linked to the sinister secrets and emotional trauma that have been passed through the generations of their family.",
             ImagePath: "https://m.media-amazon.com/images/M/MV5BOTU5MDg3OGItZWQ1Ny00ZGVmLTg2YTUtMzBkYzQ1YWIwZjlhXkEyXkFqcGdeQXVyNTAzMTY4MDA@._V1_.jpg",
             Featured: false,
-            Genre: {
-                Name: "Supernatural horror",
-                Description: "Supernatural horror film is a film genre that combines aspects of supernatural film and horror film. Supernatural occurrences in such films often include ghosts and demons, and many supernatural horror films have elements of religion."
-            },
-            Director: {
-                Name: "Ari Aster",
-                Bio: "Ari Aster is an American filmmaker. He became best known for writing and directing Hereditary, Midsommar, and Beau Is Afraid, all of which were released by A24.",
-                Birth: "1986-01-01T00:00:00.000Z"
-            },
+            Genre: "Supernatural horror",
+            Director: "Ari Aster"
         },
         {
             _id: "65bd85811532675fdd12af32",
@@ -27,15 +20,8 @@ export const MainView = () => {
             Description: "A family's serene beach vacation turns to chaos when their doppelgangers appear and begin to terrorize them.",
             ImagePath: "https://upload.wikimedia.org/wikipedia/en/0/00/Us_%282019%29_theatrical_poster.png",
             Featured: false,
-            Genre: {
-                Name: "Psychological horror",
-                Description: "Psychological horror is a subgenre of horror and psychological fiction with a particular focus on mental, emotional, and psychological states to frighten, disturb, or unsettle its audience."
-            },
-            Director: {
-                Name: "Jordan Peele",
-                Bio: "Jordan Haworth Peele is an American actor, comedian, and filmmaker. He is known for his film and television work in the comedy and horror genres. Peele started his career in sketch comedy before transitioning his career to a writer and director of psychological horror and satirical films.",
-                Birth: "1979-01-01T00:00:00.000Z"
-            }
+            Genre: "Psychological horror",
+            Director: "Jordan Peele"
         },
         {
             _id: "65bd82bb1532675fdd12af2e",
@@ -43,16 +29,9 @@ export const MainView = () => {
             Description: "When a group of rich 20-somethings plan a hurricane party at a remote family mansion, a party game turns deadly in this fresh and funny look at backstabbing, fake friends, and one party gone very, very wrong.",
             ImagePath: "https://m.media-amazon.com/images/M/MV5BYTA2ODg5ZjgtOTU2My00MzFkLWI0NzMtZmQ5MmRhMWU1NzhlXkEyXkFqcGdeQXVyMTM1MTE1NDMx._V1_.jpg",
             Featured: false,
-            Genre: {
-                Name: "Horror comedy",
-                Description: "The horror comedy subgenre is a crossover between the two genres that often utilizes dark or gallows humor and parody. It has been described as able to be categorized under three types: black comedy, parody and spoof."
-            },
-            Director: {
-                Name: "Halina Reijn",
-                Bio: "Halina Reijn is a renowned director, actress, author and producer. She is the winner of multiple awards, including the Dutch film Award, the Golden Calf for Best Actress , the Dutch theater awards Theo D'Or and Colombina, as well as the Courbois pearl, an oeuvre award which honours an actress who has made an indelible impression, both on stage as in film and television.",
-                Birth: "1975-01-01T00:00:00.000Z"
-            }
-        }
+            Genre: "Horror comedy",
+            Director: "Halina Reijn"
+        },
     ]);
 
     const [selectedMovie, setSelectedMovie] = useState(null); //initial state will be null so movie details will not be visible
