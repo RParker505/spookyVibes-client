3db870549d8ac748b0fe2bb64315442c410243c4
RParker505 <rchiado75@gmail.com>
2024-03-03 14:38:49 -0700

    Import PropTypes and define requirements

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 export const MovieView = ({movieData, onBackClick}) => {
     return (
         <div>
@@ -23,4 +25,17 @@ export const MovieView = ({movieData, onBackClick}) => {
             <button onClick={onBackClick}>Back</button>
         </div>
     );
+};
+
+//Define props constraints for MovieView
+//props object must include a movie object, which requires a title, image, director, genre and description
+MovieView.propTypes = {
+    movieData: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+        director: PropTypes.string.isRequired,
+        genre: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired
+    }).isRequired,
+    onBackClick: PropTypes.func.isRequired,//must include onBackClick and it must be a function
 };
