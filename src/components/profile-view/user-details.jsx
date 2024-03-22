80842e06581ca002e846c75012288a85df92ed5e
RParker505 <rchiado75@gmail.com>
2024-03-22 15:45:40 -0600

    Create profile-view folder and start user-details component

---
+++ src/components/profile-view/user-details.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const export UserDetails =() => {
+    return (
+        <>
+        <h2>Hey Rocky!</h2>
+        <p>Username:</p>
+        <p>Email:</p>
+        <p>Birthday:</p>
+        </>
+    );
+};
